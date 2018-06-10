e5ea5fd1995906f057698baeb2fcedfa43d9824e
Lruiyou <1607320438@qq.com>
2018-06-11 00:21:48 +0800

    设置字体大小的自定义package包

---
+++ JavaProject/src/myFont/tool.java
@@ -0,0 +1,9 @@
+package myFont;
+
+import java.awt.*;
+public class tool {
+   public static Font f1=new Font("微软雅黑",Font.PLAIN,16);
+   public static  Font f2=new Font("微软雅黑",Font.PLAIN,14);
+   public static Font f3=new Font("微软雅黑",Font.PLAIN,12);
+   public  static  Font f4=new Font("微软雅黑",Font.PLAIN,9);
+}
